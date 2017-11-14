7a8983265e8cc371c9a59f0c64472cec1c6ed153
LordAtomic <antoine27cordier@gmail.com>
2017-11-14 14:01:30 +0100

    Start Controller

---
+++ Index.php
@@ -0,0 +1,32 @@
+<?php
+include ('Src/Controleur/Controleur.php');
+$Controleur = new Controleur()
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Paris2024</title>
+
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+    <!--css bootstrap -->
+    <link rel="stylesheet" href="Web/Css/Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="Web/Css/Bootstrap/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="Web/Css/Bootstrap/css/bootstrap-reboot.css">
+
+    <!-- css font awesome -->
+    <link rel="stylesheet" href="Web/Css/Font_awesome/css/font-awesome.css">
+
+    <!-- css perso -->
+    <link rel="stylesheet" href="Web/Css/My_css/Header.css">
+</head>
+<body>
+<?php
+$Controleur->header();
+?>
+
+</body>
+</html>
